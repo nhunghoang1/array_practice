95ff41d6154cfb38b2a2560d28a2ee4f92db3b17
Mark Gandolfo <mark@gandolfo.com.au>
2018-08-27 20:08:32 +1000

    adding test exercise to learn rspec

---
+++ spec/execise000_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'spec_helper'
+
+# What is rspec?
+# It's a library that lets you test your code
+# For example:
+#
+# Make the below rspec expect statement true
+
+
+# The specs to test the exercise
+RSpec.describe 'True is True' do
+  it 'will look for true' do
+    expect(true).to eq(false)
+  end
+end
