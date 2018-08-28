428f1f6973330d48f80d2b2bd800dfeabded8073
Mark Gandolfo <mark@gandolfo.com.au>
2018-08-28 21:28:02 +1000

    made exercise00 consistent with solutions found in the rest of the exercises

--- spec/execise00_spec.rb
+++ spec/execise00_spec.rb
@@ -5,11 +5,13 @@ require_relative 'spec_helper'
 # It's a library that lets you test your code
 # For example:
 #
-# Make the below rspec expect statement true, by changing the false to true on line 13
+# Make the below rspec expect statement true, by changing the variable on line 10 to true
+
+make_this_true = false
 
 # The specs to test the exercise
 RSpec.describe 'True is True' do
   it 'will look for true' do
-    expect(true).to eq(false)
+    expect(true).to eq(make_this_true)
   end
 end
