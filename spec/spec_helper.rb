540759f97ef89e6b8ea1504a22780ab956d368eb
Mark Gandolfo <mark@gandolfo.com.au>
2018-08-28 20:08:44 +1000

    default to fast failure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 RSpec.configure do |config|
+  config.fail_fast = true
+
   config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = false
   end
 
   config.mock_with :rspec do |mocks|
